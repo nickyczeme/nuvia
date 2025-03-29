14b7d113d9d51cbf4cebeb488afce5b77eb83e77
Barbara Zoani Gray <bzoani@itba.edu.ar>
2025-03-29 10:05:03 -0300

    add drf-yasg

--- backend/urls.py
+++ backend/urls.py
@@ -18,8 +18,25 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/usuarios/', include('core.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Nuvia API",
+      default_version='v1',
+      description="Hackathon backend 💥",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns += [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+]
